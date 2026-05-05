5dfb4638f9eefdc64fcd74172169ec88fa0f14ef
annie-bw <a.bwiza1@alustudent.com>
2026-05-05 06:28:35 +0200

    Add missing Cacheable import

--- backend/src/main/java/com/provx/driving_test/Repository/QuestionRepository.java
+++ backend/src/main/java/com/provx/driving_test/Repository/QuestionRepository.java
@@ -4,6 +4,7 @@ import com.provx.driving_test.models.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
